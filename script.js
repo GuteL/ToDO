3103777fc55d0abc167a0e2bc5c5eee443d1cbd8
GuteL <leon.gute4@gmail.com>
2025-03-19 14:27:16 +0000

    Update

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-const input = document.querySelector("#toDoEingabe");
-const add = document.querySelector("#toDoHinzufügen");
+const inputToDo = document.getElementById("toDoEingabe");
+const addButton = document.getElementById("toDoHinzufügen");
 
 
+addButton.addEventListener("click", () => {
+    
+    // strings aus dem localstorage in js-objekte parsen
+    // JSON.parse()
+    // Items für den LocalStorage in string umwandeln
+    // JSON.stringify()
+    
+    const todoItemsString = localStorage.getItem("todo-app");
+    
+    const todoItems = todoItemsString !== null ? JSON.parse(todoItemsString) : [];
+    
+    const inputElementValue = inputToDo.value;
+    
+    const newToDo = { description: inputElementValue, checked: false };
+    
+    todoItems.push(newToDo);
+    
+    localStorage.setItem("todo-app", JSON.stringify(todoItems));
+});
 
-const todo = [{ description: "input", checked: false }];
- 
-// strings aus dem localstorage in js-objekte parsen
-// JSON.parse()
-// Items für den LocalStorage in string umwandeln
-// JSON.stringify()
- 
-localStorage.setItem("todo-app", JSON.stringify(todo));
- 
-const todoItemsString = localStorage.getItem("todo-app");
- 
-const todoItems = todoItemsString !== null ? JSON.parse(todoItemsString) : [];
- 
-const inputElementValue = 'new'
- 
-const newToDo = { description: inputElementValue, checked: false };
- 
-todoItems.push(newToDo);
- 
-localStorage.setItem("todo-app", JSON.stringify(todo));
- 
- 
 // leere die view komplett, lese den localstorage erneut aus, rendere die vollständige todo-liste wieder in die view
